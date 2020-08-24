fcbbb03deef18e46fa1c712fc42e9a66a099d43f
Adrien Aminta <adrien.aminta@gmail.com>
2020-08-24 10:35:51 +0200

    incremental search working for all 5 departure fields

--- public/script.js
+++ public/script.js
@@ -8,14 +8,6 @@ $(".close").on("click", function () {
 });
 
 (function () {
-    /* let inputField;
-    for (var i = 0; i < 5; i++) {
-        inputField = $([".departure" + i]);
-        console.log("inputfield after loop", inputField);
-    }
-
-    console.log("let inputField", inputField); */
-    var inputField = $(".departure");
     var cities = [
         "Aberdeen",
         "Alicante",
@@ -47,59 +39,73 @@ $(".close").on("click", function () {
         "Verona",
         "Vilnius",
     ];
-    var resultsContainer = $("#results-container");
-    var arrowDown;
 
-    inputField.on("input", function () {
-        var inputValue = inputField.val();
-        console.log(inputValue);
-        var results = [];
-        if (inputValue === "") {
-            results = [];
-        } else {
-            for (var i = 0; i < cities.length; i++) {
-                if (
-                    cities[i]
-                        .toLowerCase()
-                        .indexOf(inputValue.toLowerCase()) === 0
-                ) {
-                    results.push(cities[i]);
-                }
-                if (results.length === 4) {
-                    break;
+    // let inputField;
+    for (let i = 0; i < 5; i++) {
+        let inputField = $(".departure" + i);
+        // console.log("inputfield after loop", inputField);
+
+        // var inputField = $(".departure");
+        // ===> SEE use let: Einfluss bei loop: Hier wenn let dann kriegen wir jeden einzelnen resultscontainer.
+        // ===> Während mit var nicht dann  kriegen mit den container entweder ganz oben oder ganz unten.
+        let resultsContainer = $("#results-container" + i);
+        var arrowDown;
+
+        inputField.on("input", function () {
+            var inputValue = inputField.val();
+            console.log(inputValue);
+            var results = [];
+            if (inputValue === "") {
+                results = [];
+            } else {
+                for (var j = 0; j < cities.length; j++) {
+                    if (
+                        cities[j]
+                            .toLowerCase()
+                            .indexOf(inputValue.toLowerCase()) === 0
+                    ) {
+                        results.push(cities[j]);
+                    }
+                    if (results.length === 4) {
+                        break;
+                    }
                 }
             }
-        }
 
-        var resultsHtml = "";
-        if (results.length === 0) {
-            console.log(results);
-            resultsHtml = "<p>No results for your departure city</p>";
-        }
-        for (var j = 0; j < results.length; j++) {
-            resultsHtml += "<p class='city-search'>" + results[j] + "</p>";
-        }
+            var resultsHtml = "";
+            if (results.length === 0) {
+                console.log(results);
+                resultsHtml = "<p>No results for your departure city</p>";
+            }
+            for (var q = 0; q < results.length; q++) {
+                resultsHtml += "<p class='city-search'>" + results[q] + "</p>";
+            }
 
-        if (inputValue === "") {
-            resultsHtml = "";
+            if (inputValue === "") {
+                resultsHtml = "";
 
-            arrowDown = undefined;
-        }
+                arrowDown = undefined;
+            }
 
-        resultsContainer.html(resultsHtml);
-    });
-    resultsContainer.on("mouseover", "p", function () {
-        var p = $(this);
-    });
-    resultsContainer.on("mouseout", "p", function () {
-        var p = $(this);
-    });
+            console.log(resultsContainer);
 
-    resultsContainer.on("mousedown", "p", function () {
-        var p = $(this);
-        inputField.val(p.html());
-        resultsContainer.html("");
-    });
+            resultsContainer.html(resultsHtml);
+        });
+        resultsContainer.on("mouseover", "p", function () {
+            var p = $(this);
+        });
+        resultsContainer.on("mouseout", "p", function () {
+            var p = $(this);
+        });
+
+        resultsContainer.on("mousedown", "p", function () {
+            var p = $(this);
+            inputField.val(p.html());
+            resultsContainer.html("");
+        });
+    }
+
+    /*     console.log("let inputField", inputField); */
 })();
 //click().find(".bg-modal").css("display", "flex");
 /*     console.log("connection with clickfunctions")
